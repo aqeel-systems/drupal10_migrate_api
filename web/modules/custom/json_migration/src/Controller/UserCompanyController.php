584eb781335c3bd25a6905560bf4f648d6c9e5a6
Aqeel Ur Rahman <aqeel261@hotmail.com>
2024-10-01 19:32:05 +0500

    Company listing fix

--- web/modules/custom/json_migration/src/Controller/UserCompanyController.php
+++ web/modules/custom/json_migration/src/Controller/UserCompanyController.php
@@ -9,16 +9,38 @@ use Drupal\node\Entity\Node;
 class UserCompanyController extends ControllerBase {
 
   public function display() {
-    $users = User::loadMultiple();
+    $users = User::loadMultiple();  // Load all users
     $output = '<h2>Users and their Companies</h2><ul>';
-    foreach ($users as $user) {
-      $company_name = $user->get('field_company_name')->value;
-      $output .= '<li>' . $user->getUsername() . ' works at ' . $company_name . '</li>';
+
+    foreach ($users as $index => $user) {
+
+      // Check if the user has the field_company field and it's not empty
+      if ($user->hasField('field_company') && !$user->get('field_company')->isEmpty()) {
+        // Load the referenced company entity (assuming field_company is a reference to node:company)
+        $company_ref = $user->get('field_company')->entity;
+
+        if ($company_ref && $company_ref->hasField('field_company_name')) {
+          // Access the company name field
+          $company_name = $company_ref->get('field_company_name')->value;
+        } else {
+          $company_name = 'Unknown Company';
+        }
+      } else {
+        $company_name = 'No Company';
+      }
+      if ($index > 0){
+        // Add the user and company information to the output
+        $output .= '<li>' . $user->getAccountName() . ' works at ' . $company_name . '</li>';
+      }
+
     }
+
     $output .= '</ul>';
+
     return [
       '#markup' => $output,
     ];
   }
+
 
 }
